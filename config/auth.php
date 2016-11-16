b07fb6262a1ecc8e8617a27faebf9812ed33e1b9
namikaze <b5216@oic.jp>
2016-11-16 11:07:33 +0900

    hi!

--- config/auth.php
+++ config/auth.php
@@ -37,12 +37,12 @@ return [
 
     'guards' => [
         'web' => [
-            'driver' => 'session',
+            'driver' => 'database',
             'provider' => 'users',
         ],
 
         'api' => [
-            'driver' => 'token',
+            'driver' => 'database',
             'provider' => 'users',
         ],
     ],
